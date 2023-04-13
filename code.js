27014cf16b566fd93cb82df48963bfd9bef55db2
Sibusiso Vincent <51314855+vultureofficial@users.noreply.github.com>
2023-04-14 01:37:02 +0200

    initial commit

---
+++ code.js
@@ -0,0 +1,29 @@
+
+
+const file_main_c =[
+"#include <iostream>",
+" ",
+" ",
+"int main() {",
+'    std::cout << 1 + 1 << std::endl;',
+"    return 0;",
+"}"
+ ] ; 
+
+
+ const files = [
+    "a.out",
+    "main.cpp",
+    "Readme.md",
+    "projects@gitrepo"
+ ];
+
+
+ const about = [
+    "                       OS8 by Gama Sibusiso",
+    "               forked from  NanoShell (April) 2023\n", 
+    "  ",
+    "   ", 
+    "                visit: github.com/vultureofficial/OS8", 
+    "  "
+ ];
